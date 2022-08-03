6483ae1a0d810104a8b3d3608d0240e3e99dfcc4
Thinh Do <dqthinh69752@gmail.com>
2022-08-03 10:58:13 +0700

    material and flicker
    - update logic handling tile's true state when flickered and triggered
    - add new default material for tile (white with black border)

--- Assets/Scripts/Objects/Tile.cs
+++ Assets/Scripts/Objects/Tile.cs
@@ -6,21 +6,41 @@ public class Tile : MonoBehaviour
 {
     public Color TileColor { get; protected set; }
 
+    private bool _isTriggered;
+    private bool _isFlickered;
+
+    public bool IsFlickered() => _isFlickered;
+
+    public bool IsTriggered() => _isTriggered;
+
     public void FlickTrueTile(float duration)
     {
+        if (IsTriggered()) return;
+
         float delay = 0.1f;
         Invoke("ShowTrueTile", delay);
         Invoke("HideTrueTile", delay + duration);
+
+        _isFlickered = true;
+        StartCoroutine(SetFlickerStateCoroutine(false, delay + duration));
     }
 
     public void OnTriggered()
     {
-        ShowTrueTile();
+        if (IsFlickered()) return;
+
+        if (IsTriggered())
+            HideTrueTile();
+        else
+            ShowTrueTile();
+        
+        _isTriggered = !_isTriggered;
     }
 
     protected virtual void Start()
     {
         TileColor = Color.white;
+        _isTriggered = false;
     }
 
     protected virtual void ShowTrueTile()
@@ -31,5 +51,11 @@ public class Tile : MonoBehaviour
     protected virtual void HideTrueTile()
     {
         // Do nothing for base tile
+    }
+
+    private IEnumerator SetFlickerStateCoroutine(bool state, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        _isFlickered = state;
     }
 }
