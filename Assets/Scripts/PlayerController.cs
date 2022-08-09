83581ade0b02b81665cdd3f2d0418c3707e4f72b
Thinh Do <dqthinh69752@gmail.com>
2022-08-09 15:08:21 +0700

    player controller update
    - smooth player's movement through velocity manipulation
    - refactor codes
    - add dummy shapes for walls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,7 +10,11 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] private float accelerationRate = 1.0f;     // speed per second
 
+    [SerializeField] private float decelerationRate = 2.0f;    // speed per second
+
     [SerializeField] private float decelerateWaitTime = 0.5f;   // waiting time for the current speed to be reset (after no input from users)
+
+    //private bool isTouchDevice;
 
     private float currentSpeed;
 
@@ -18,7 +22,15 @@ public class PlayerController : MonoBehaviour
 
     private float decelerateTiming;
 
+    private bool isMouseDragged;
+
+    private bool isGrounded;
+
+    private Vector3 mouseStartPosition;
+
     private Rigidbody rb;
+
+    private Vector3 vectorZero;
 
     private void Awake()
     {
@@ -27,9 +39,28 @@ public class PlayerController : MonoBehaviour
 
     private void Start()
     {
+        vectorZero = Vector3.zero;
+
         currentSpeed = startingSpeed;
         decelerateFlag = false;
         decelerateTiming = decelerateWaitTime;
+        isMouseDragged = false;
+        isGrounded = false;
+
+        mouseStartPosition = vectorZero;
+    }
+
+    private void FixedUpdate()
+    {
+        if (isGrounded && rb.velocity.y != 0)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        }
+
+        if (decelerateFlag)
+        {
+            rb.velocity *= Mathf.Clamp(1 - Time.fixedDeltaTime * decelerationRate, 0, 1);
+        }
     }
 
     private void Update()
@@ -37,6 +68,66 @@ public class PlayerController : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
+        if (horizontalInput == 0 && verticalInput == 0)
+        {
+            ProcessMouseInput(ref horizontalInput, ref verticalInput);
+        }
+        
+        ProcessMovement(horizontalInput, verticalInput);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void ProcessMouseInput(ref float horizontalInput, ref float verticalInput)
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!isMouseDragged)
+            {
+                mouseStartPosition = Input.mousePosition;
+            }
+            isMouseDragged = true;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isMouseDragged = false;
+        }
+
+        if (isMouseDragged)
+        {
+            Vector3 currentMousePosition = Input.mousePosition;
+
+            horizontalInput = GetMouseHorizontalInput(currentMousePosition);
+            verticalInput = GetMouseVerticalInput(currentMousePosition);
+        }
+    }
+
+    private float GetMouseHorizontalInput(Vector3 currentMousePosition)
+    {
+        float minInputValue = -1f;
+        float maxInputValue = 1f;
+
+        float horizontalDelta = currentMousePosition.x - mouseStartPosition.x;
+        return Mathf.Clamp(horizontalDelta, minInputValue, maxInputValue);
+    }
+
+    private float GetMouseVerticalInput(Vector3 currentMousePosition)
+    {
+        float minInputValue = -1f;
+        float maxInputValue = 1f;
+
+        float verticalDelta = currentMousePosition.y - mouseStartPosition.y;
+        return Mathf.Clamp(verticalDelta, minInputValue, maxInputValue);
+    }
+
+    private void ProcessMovement(float horizontalInput, float verticalInput)
+    {
         bool receivedInput = (horizontalInput != 0 || verticalInput != 0);
 
         if (currentSpeed > startingSpeed)
@@ -58,16 +149,18 @@ public class PlayerController : MonoBehaviour
             if (decelerateTiming <= 0)
             {
                 decelerateFlag = false;
-                currentSpeed = startingSpeed;
                 decelerateTiming = decelerateWaitTime;
+
+                currentSpeed = startingSpeed;
             }
         }
 
         if (receivedInput)
         {
             currentSpeed = Mathf.Clamp(currentSpeed + Time.deltaTime * accelerationRate, startingSpeed, maxSpeed);
-        }
 
-        transform.Translate(horizontalInput * Time.deltaTime * currentSpeed, 0, verticalInput * Time.deltaTime * currentSpeed);
+            Vector3 appliedForce = new Vector3(horizontalInput * Time.deltaTime * currentSpeed, 0, verticalInput * Time.deltaTime * currentSpeed);
+            rb.AddForce(appliedForce, ForceMode.VelocityChange);
+        }
     }
 }
